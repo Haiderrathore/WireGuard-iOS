6ae5e0c167bfee9413729242c2ea495e2b56828c
Haiderrathore <haiderrathore93@gmail.com>
2024-12-26 17:50:14 +0500

    initial commit 2

---
+++ Wireguard-Solution/Wireguard-Solution/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Wireguard-Solution
+//
+//  Created by Haider on 18/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
